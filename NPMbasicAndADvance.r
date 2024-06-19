7fec54c7c1c298d0ff130d02fe670462c43c818e
RAGHURAJ PRATAP SINGH <136258185+raghurajsingh8@users.noreply.github.com>
2024-06-20 01:29:32 +0530

    Add files via upload
    npm run use for runprogram
    npm start use for open file

---
+++ NPMbasicAndADvance.r
@@ -0,0 +1,40 @@
+_______________________NPM understanding
+node package manager
+node se related sare package 
+node js core mein jo installed aata hai wo kehlaaata hai __module
+ aur jo npm se download krte hai vo __package hota hai 
+
+
+text to speech npm we can use easily 
+_______________________________installing and uninstalling anything basic and advanced______________
+
+
+install--> npm i package_name
+uninstall--> npm uninstall package_name
+
+search package herre https://www.npmjs.com/
+
+install versing using @ 
+npm i accessibility@2.0.9
+
+__dependecies- packages and packages kit dependencies
+__devdependencies :- aise package jo sirf development mein kaam ayenge par jab app bnn jayenge aur upload ho jayenge to hm inhe use nhi kr rhe honge 
+
+__npm start(open file) __num run dev (console type things )and similer this   kyo likhte hai  
+
+____importent__
+
+package.json me 
+"scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "node script.js"  __khud ka bna skte  npm start hain ,
+    "chacha":"echo \"this is good\"" npm run chacha 
+  },
+
+> npm run chacha
+
+> advancejavascript@1.0.0 chacha
+> echo "this is good"
+
+"this is good"
+
