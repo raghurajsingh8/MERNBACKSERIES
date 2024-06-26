d0038382f6d1fb11a30c4c5977a322a9e72c06ca
RAGHURAJ PRATAP SINGH <136258185+raghurajsingh8@users.noreply.github.com>
2024-06-26 16:16:13 +0530

    Add files via upload

---
+++ mongotheory.sh
@@ -0,0 +1,60 @@
+Data storage:
+aisa kuch jha pr ham saare user i=ki info store kr skte hai 
+file data.txt
+
+but file mekeval text hoga image song nhi ho payega
+
+
+
+concept of DATABASE it can store any thing what you want 
+
+Types of database
+sql data ko table ke roop me use krte hai  
+
+no sql isme object hote hai aur object ke roop me data hota hai exmple mongodb we have to study here 
+
+
+tree is a concept mangho is a tree 
+
+no sql is a concept and mongodb is a no sql db
+
+__terminologies__ 
+__________________________________________
+ | 
+ |in backend there is 2 thingsapp server & data base    server 
+ |
+ |
+_|_________________________________________
+
+application server -->node
+database server-->mongodb
+
+
+_________________________________________________
+Application server
+
+route handle krta hai req ka res krta hai 
+data ke alava saara kaam krna 
+
+AS req accept krta hai to jrurat hone pr db server pr req jaaati hai 
+
+
+
+
+__________________________________________________
+database server 
+ 
+ ek project ke jitne data hote hai collection kahlate hai 
+
+ jaise ek  project me contact review ka data rakha hai to inhe collection khenege
+
+
+ _______________________________________________ 
+ database--->collection-->document(1 user data)
+
+
+ moongoose.connect->database create;
+
+ model create  ->colllection
+
+ CREATE code--->document
